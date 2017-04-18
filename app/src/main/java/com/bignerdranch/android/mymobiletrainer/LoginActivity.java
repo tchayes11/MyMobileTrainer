a38e964d90a66529904952b63705ed3bea58fc86
TinaCH <thayes9@live.spcollege.edu>
2017-04-18 14:18:26 -0400

    redoAssignment3

--- app/src/main/java/com/bignerdranch/android/mymobiletrainer/LoginActivity.java
+++ app/src/main/java/com/bignerdranch/android/mymobiletrainer/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.bignerdranch.android.mymobiletrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.view.KeyEvent;
+
 
 public class LoginActivity extends AppCompatActivity {
+    private Button mLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+       /* TextView textUsername = (TextView)findViewById(R.id.textUsername);
+        textUsername.getText();*/
     }
-}
+
+
+        }
+
+
+
+
+
